1dd72438e41066c07f46bd86de52564174126833
Josh Kuhar <jdkuhar@gmail.com>
2016-09-19 11:54:58 -0400

    update

--- config.js
+++ config.js
@@ -1,6 +1,11 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
-                       (process.env.NODE_ENV === 'production' ?
-                            'mongodb://localhost/shopping-list' :
+                       (process.env.MONGODB_URI === 'production' ?
+                       	//process.env.NODE_ENV === 'production' ?
+                       		'mongodb://<dbuser1>:<dbuser1>@ds035776.mlab.com:35776/jk-shopping-list' :
+                            //'mongodb://localhost/shopping-list' :
                             'mongodb://localhost/shopping-list-dev');
 exports.PORT = process.env.PORT || 8080;
+
+//process.env.MONGODB_URL
+//mongodb://<dbuser1>:<dbuser1>@ds035776.mlab.com:35776/jk-shopping-list
